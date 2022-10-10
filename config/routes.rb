f33a52b76e92dc099bd638d07aaeefda116c64e0
NarcisseObadiah <mitokpenarcisseobadiah>
2022-10-10 14:05:18 +0200

    Adding the posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+   resources:posts
   # Defines the root path route ("/")
   root "pages#home"
 end
